77edd22d5188bb37ce282d62d50543ecfe699520
unknown <kacky758@gmail.com>
2022-09-07 12:03:38 +0900

    fs

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ const data = bgImage();
           {data}
         </div>
           <div className="pt-32 z-10 ">
-            <img src='bandlogo2.png' width="600" height="300" objectFit="contain" alt=""/>
+            <img src='/bandlogo2.png' width="600" height="300" objectFit="contain" alt=""/>
           </div>
           <div className="mx-auto hidden md:inline-block justify-center items-center mb-32">
             <iframe width="560" height="315" src="https://www.youtube.com/embed/bLgcQ-oJbu8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
